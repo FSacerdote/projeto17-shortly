b235ec4af77ac3280a0c120a1ef7e700710342ed
Fernando_Sacerdote <fernando.saerdote@gmail.com>
2023-08-03 16:55:36 -0300

    feat: posy user

--- src/controllers/auth.crontrollers.js
+++ src/controllers/auth.crontrollers.js
@@ -1,5 +1,16 @@
-export async function signup (req, res){
+import { db } from "../database/database.connection.js"
 
+export async function signup (req, res){
+    const {name, email, password, confirmPassword} = req.body
+    try {
+        if(password !== confirmPassword) return res.sendStatus(422)
+        const user = await db.query(`SELECT * FROM users WHERE email = $1;`, [email])
+        if(user.rowCount) return res.sendStatus(409)
+        db.query(`INSERT INTO users (name, email, password) VALUES ($1,$2,$3);`, [name, email, password])
+        res.send(201)
+    } catch (error) {
+       res.status(500).send(error.message) 
+    }
 }
 
 export async function signin (req, res){

4b1e74aa8c6ddb914374e6ef2d296a4c979f43e7
Fernando_Sacerdote <fernando.saerdote@gmail.com>
2023-08-05 16:18:26 -0300

    feat: shorten route

--- src/controllers/urls.controllers.js
+++ src/controllers/urls.controllers.js
@@ -1,5 +1,23 @@
-export async function shorten(req, res){
+import { db } from "../database/database.connection.js"
+import { nanoid } from "nanoid"
 
+export async function shorten(req, res){
+    const {url} = req.body
+    const {authorization} = req.headers
+    const token = authorization?.replace("Bearer ", "")
+    
+    if (!token) return res.sendStatus(401)
+    try {
+        const validation =  await db.query(`SELECT * FROM sessions WHERE token=$1`, [token])
+        if (!validation.rowCount) return res.sendStatus(401)
+        const user = validation.rows[0].userid
+        const shortUrl =  nanoid()
+        await db.query(`INSERT INTO urls (shorturl, url, userid) VALUES ($1,$2,$3)`, [shortUrl, url, user])
+        const response = await db.query(`SELECT * FROM urls WHERE shorturl=$1`, [shortUrl])
+        res.status(201).send({id: response.rows[0].id,shortUrl})
+    } catch (error) {
+        res.status(500).send(error.message)
+    }
 }
 
 export async function getUrlById(req, res){
--- src/routes/urls.routes.js
+++ src/routes/urls.routes.js
@@ -1,8 +1,11 @@
 import { Router } from "express"
+import validateSchema from "../middlewares/validateSchema.js"
+import { urlSchema } from "../schemas/urls.schemas.js"
+import { shorten } from "../controllers/urls.controllers.js"
 
 const urlsRouter = Router()
 
-urlsRouter.post("/urls/shorten")
+urlsRouter.post("/urls/shorten", validateSchema(urlSchema), shorten)
 urlsRouter.get("/urls/:id")
 urlsRouter.get("/urls/open/:shortUrl")
 urlsRouter.delete("/urls/:id")

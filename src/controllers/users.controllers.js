9b886959e2d978a15f059366c01befbaf9f7d455
Fernando_Sacerdote <fernando.saerdote@gmail.com>
2023-08-05 18:34:59 -0300

    feat: getUser

--- src/controllers/users.controllers.js
+++ src/controllers/users.controllers.js
@@ -1,5 +1,23 @@
-export async function getUser(req, res){
+import { db } from "../database/database.connection.js"
 
+export async function getUser(req, res){
+    const {authorization} = req.headers
+    const token = authorization?.replace("Bearer ", "")
+    if(!token) return res.sendStatus(401)
+    try {
+        const userResponse = await db.query(`SELECT * FROM sessions WHERE token=$1;`, [token])
+        if(!userResponse.rowCount) return res.sendStatus(401)
+        const userId = userResponse.rows[0].userid
+        const user = await db.query(`
+            SELECT users.id, users.name, COALESCE(CAST(SUM(urls.visitcount) AS INTEGER), 0) AS "visitCount" FROM users 
+            LEFT JOIN urls ON users.id = urls.userid  
+            WHERE users.id=$1 GROUP BY users.id;
+            `, [userId])
+        const urls = await db.query(`SELECT id, shorturl, url, visitcount FROM urls WHERE urls.userid=$1`, [userId])
+        res.send({...user.rows[0], shortenedUrls: urls.rows})
+    } catch (error) {
+     res.status(500).send(error.message)
+    }
 }
 
 export async function getRanking(req, res){
--- src/routes/users.routes.js
+++ src/routes/users.routes.js
@@ -1,8 +1,9 @@
 import { Router } from "express"
+import { getUser } from "../controllers/users.controllers.js"
 
 const usersRouter = Router()
 
-usersRouter.get("/users/me")
+usersRouter.get("/users/me", getUser)
 usersRouter.get("/ranking")
 
 export default usersRouter
